878872a38f031ffc3a25e386ac653a2a75b5fd0d
Maibritt Rittermann <mri@skivecollege.dk>
2022-10-12 14:15:26 +0200

    small update

--- routes/predict.js
+++ routes/predict.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const predict = require('../services/predictService');
+const isImageURL = require('image-url-validator').default;
 
 router.post('/', [auth], async (req, res) => {
     
-    // Todo: validate imageURL and image
-    const prediction = await predict(req.body.filename);
+    const imageURL = await isImageURL(req.body.filename);
+    const prediction = await predict(imageURL);
     if(!prediction) return res.status(404).send("Ingen forudsigelser om billedet");
     res.send(prediction);
 });
